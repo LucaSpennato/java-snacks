5b20222b16e5b110896f097f87827de1d0ab52c7
Luca Spennato <luca.spennato@gmail.com>
2022-11-16 09:39:25 +0100

    snack 1

---
+++ src/snacks/days/one/Snack1.java
@@ -0,0 +1,21 @@
+package snacks.days.one;
+
+import java.util.Scanner;
+
+public class Snack1 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int catchNumber = sc.nextInt();
+		
+		if(catchNumber %2 == 0) {
+			System.out.println("Even!: " + catchNumber);
+		}else {
+			System.out.println("Let's fix that: " + (catchNumber + 1));
+		}
+		
+	}
+	
+}
